78fb6a691196ca484fea365f694e96021e27eb0d
SOuellette22 <143441012+SOuellette22@users.noreply.github.com>
2024-10-04 12:45:14 -0400

    fix: the grading system is working correctly now

--- ArticlePractice.py
+++ ArticlePractice.py
@@ -1,6 +1,6 @@
 import random as r
 
-wordsAll = [# Male section of words
+wordsAll = [# Male section of words Singular
             ["Amico", "un", "l\'", "M"], ["Ragazzo", "un", "il", "M"], ["Bambino", "un", "il", "M"],
             ["Frantello", "un", "il", "M"], ["Cappuccino", "un", "il", "M"], ["Gatto", "un", "il", "M"],
             ["Giorno", "un", "il", "M"], ["Libro", "un", "il", "M"], ["Albergo", "un", "l\'", "M"],
@@ -12,6 +12,16 @@ wordsAll = [# Male section of words
             ["Simpatico", "un", "il", "M"], ["Sportivo", "uno", "lo", "M"], ["Pigro", "un", "il", "M"],
             ["Timido", "un", "il", "M"], ["Bravo", "un", "il", "M"], ["Creativo", "un", "il", "M"],
             ["Onesto", "un", "l\'", "M"], ["Estroverso", "un", "l\'", "M"],
+            
+            # Male section of words Plural
+            ["Amici", "i", "M"], ["Ragazzi", "i", "M"], ["Bambini", "i", "M"],
+            ["Frantelli", "i", "M"], ["Cappuccini", "i", "M"], ["Gatti", "i", "M"],
+            ["Giorni", "i", "M"], ["Libri", "i", "M"], ["Alberghi", "gli", "M"],
+            ["Piatti", "i", "M"], ["Zii", "gli", "M"], ["Zaini", "gli", "M"],
+            ["Professori", "i", "M"], ["Studenti", "gli", "M"], ["Dottori", "i", "M"],
+            ["Padri", "i", "M"], ["Bicchieri", "i", "M"], ["Latti", "i", "M"],
+            ["Cani", "i", "M"], ["Mesi", "i", "M"], ["Cinema", "i", "M"],
+            ["Caffe\'", "i", "M"], ["Te\'", "i", "M"],
             
             # Female section fo words
             ["Amica", "un\'", "l\'", "F"], ["Ragazza", "una", "la", "F"], ["Bambina", "una", "la", "F"],
@@ -41,10 +51,13 @@ else:
 r.shuffle(wordsAll)
 
 count = 0
+countP = 0
+countS = 0
 
 correctmf = 0
 correctun = 0
 correctil = 0
+correctilPlural = 0
 
 def guesting(flag, n):
     if (flag == "yes"):
@@ -54,39 +67,72 @@ def guesting(flag, n):
             a = input("What is the \"un\" article (un,uno,una,un\'):\n")
         elif (n == 2):
             a = input("What is the \"il\" article (il,lo,la,l\'):\n")
-        if (wordsAll[i][n] == a.upper() or
-            wordsAll[i][n] == a.lower()):
+        if (wordsAll[i][n].lower() == a.lower()):
             print("Correct!!!\n")
             return True
         else:
             print("Wrong\n")
+            return False
+        
+def guestingPlurals(flag, n):
+    if(flag == "yes"):
+        if(n == 2):
+            a = input("What is the words gender:\n")
+        elif(n == 1):
+            a = input("what is the the \"the\" article plural:\n")
+        if(wordsAll[i][n].lower() == a.lower()):
+            print("Correct!!!")
+            return True
+        else:
+            print("Wrong")
             return False
 
 print()
 
 mfFlag = input("Do you want to study the gender of the words? Only yes or no\n")
 print()
-unFlag = input("Do you want to study the a/an articles (uno,un,una,un')? Only yes or no\n")
+unFlag = input("Do you want to study the \"a/an\" articles (uno,un,una,un')? Only yes or no\n")
 print()
-ilFlag = input("Do you want to study the the articles (il,lo,la,l')? Only yes or no\n")
+ilFlag = input("Do you want to study the \"the\" articles (il,lo,la,l')? Only yes or no\n")
+print()
+pluralIlFlag = input("Do you want to study the plural \"the\" article (i, gli, le)? Only yes or no\n")
 
 print()
 
 for i in range(num):
-    print("Word num " + str(i + 1) + ": " + wordsAll[i][0])
-    count += 1
-    if(guesting(mfFlag, 3)):
-        correctmf += 1
-    print("Word num " + str(i + 1) + ": " + wordsAll[i][0])     
-    if(guesting(unFlag, 1)):
-        correctun += 1
-    print("Word num " + str(i + 1) + ": " + wordsAll[i][0])
-    if(guesting(ilFlag, 2)):
-        correctil += 1
+    if (len(wordsAll[i]) == 4):
+        countS += 1
+        count += 1
+        if(mfFlag == "yes"):
+            print("Word num " + str(i + 1) + ": " + wordsAll[i][0])
+            if(guesting(mfFlag, 3)):
+                correctmf += 1
+        if(unFlag == "yes"):
+            print("Word num " + str(i + 1) + ": " + wordsAll[i][0])     
+            if(guesting(unFlag, 1)):
+                correctun += 1
+        if(ilFlag == "yes"):      
+            print("Word num " + str(i + 1) + ": " + wordsAll[i][0])
+            if(guesting(ilFlag, 2)):
+                correctil += 1
+            
+    if (len(wordsAll[i]) == 3):
+        countP += 1
+        count += 1
+        if(mfFlag == "yes"):
+            print("Word num " + str(i + 1) + ": " + wordsAll[i][0])
+            if(guestingPlurals(mfFlag, 2)):
+                correctmf += 1
+        if(pluralIlFlag == "yes"):
+            print("Word num " + str(i + 1) + ": " + wordsAll[i][0])
+            if(guestingPlurals(pluralIlFlag, 1)):
+                correctilPlural += 1
 
 if (mfFlag == "yes"):
     print("You got a " + str(round((correctmf / count) * 10000) / 100) + "% with " + str(correctmf) + " correct out of " + str(count))
 if (unFlag == "yes"):
-    print("You got a " + str(round((correctun / count) * 10000) / 100) + "% with " + str(correctun) + " correct out of " + str(count))
+    print("You got a " + str(round((correctun / countS) * 10000) / 100) + "% with " + str(correctun) + " correct out of " + str(countS))
 if (ilFlag == "yes"):
-    print("You got a " + str(round((correctil / count) * 10000) / 100) + "% with " + str(correctil) + " correct out of " + str(count))
+    print("You got a " + str(round((correctil / countS) * 10000) / 100) + "% with " + str(correctil) + " correct out of " + str(countS))
+if (pluralIlFlag == "yes"):
+    print("You got a " + str(round((correctilPlural / countP) * 10000) / 100) + "% with " + str(correctil) + " correct out of " + str(countP))
